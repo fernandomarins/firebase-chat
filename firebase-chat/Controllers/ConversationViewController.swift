6a2df3c9a06cbbfefc68ee708568248a0d5cdfb1
Fernando Marins <fernandomarins@Fernandos-MacBook-Pro.local>
2021-12-15 06:36:19 -0300

    first commit

---
+++ firebase-chat/Controllers/ConversationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConversationViewController.swift
+//  firebase-chat
+//
+//  Created by Fernando Marins on 15/12/21.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
